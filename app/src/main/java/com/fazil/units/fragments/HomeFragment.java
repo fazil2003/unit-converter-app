5fc48ce8784aa7ce5dbd2e4678a44e37c7f304b6
Mohamed Fazil Z <mohamedfazil463@gmail.com>
2023-09-30 14:04:42 +0530

    [F] Added the ConvertFragment.

--- app/src/main/java/com/fazil/units/fragments/HomeFragment.java
+++ app/src/main/java/com/fazil/units/fragments/HomeFragment.java
@@ -2,19 +2,38 @@ package com.fazil.units.fragments;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fazil.units.HomeActivity;
 import com.fazil.units.R;
 
 public class HomeFragment extends Fragment {
 
+    CardView unitArea;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        unitArea = view.findViewById(R.id.cardview_unit_area);
+        unitArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment selectedFragment = new ConvertFragment();
+                Bundle arguments = new Bundle();
+                arguments.putString("unit", "area");
+                selectedFragment.setArguments(arguments);
+                ((HomeActivity) requireActivity()).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment).addToBackStack(null).commit();
+            }
+        });
+
+        return view;
     }
 }
