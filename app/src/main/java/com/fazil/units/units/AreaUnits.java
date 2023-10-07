c3c67ac5c860c9cbb29b753ef08231337b1896ca
Mohamed Fazil Z <mohamedfazil463@gmail.com>
2023-10-07 16:01:14 +0530

    [M] Arranged the Area units in alphabetical order.

--- app/src/main/java/com/fazil/units/units/AreaUnits.java
+++ app/src/main/java/com/fazil/units/units/AreaUnits.java
@@ -5,16 +5,16 @@ import android.content.Context;
 public class AreaUnits implements Units {
 
     public static String[] unitItems = {
-            "Square Inch",
-            "Square Foot",
-            "Square Yard",
-            "Square Mile",
             "Acre",
             "Hectare",
-            "Square Millimeter",
             "Square Centimeter",
-            "Square Meter",
+            "Square Foot",
+            "Square Inch",
             "Square Kilometer",
+            "Square Meter",
+            "Square Mile",
+            "Square Millimeter",
+            "Square Yard",
     };
 
     public static String primaryUnit = "Square Meter";
@@ -24,35 +24,35 @@ public class AreaUnits implements Units {
     public double convertQuestionToCommon(String unit, Double value){
         double result = 0.0;
         switch(unit){
-            case "Square Inch":
-                result = value * 0.00064516;
-                break;
-            case "Square Foot":
-                result = value * 0.09290304;
-                break;
-            case "Square Yard":
-                result = value * 0.83612736;
-                break;
-            case "Square Mile":
-                result = value * 2589988.110336;
-                break;
             case "Acre":
                 result = value * 4046.8564224;
                 break;
             case "Hectare":
                 result = value * 10000;
                 break;
-            case "Square Millimeter":
-                result = value * 0.000001;
-                break;
             case "Square Centimeter":
                 result = value * 0.0001;
+                break;
+            case "Square Foot":
+                result = value * 0.09290304;
+                break;
+            case "Square Inch":
+                result = value * 0.00064516;
+                break;
+            case "Square Kilometer":
+                result = value * 1000000;
                 break;
             case "Square Meter":
                 result = value;
                 break;
-            case "Square Kilometer":
-                result = value * 1000000;
+            case "Square Mile":
+                result = value * 2589988.110336;
+                break;
+            case "Square Millimeter":
+                result = value * 0.000001;
+                break;
+            case "Square Yard":
+                result = value * 0.83612736;
                 break;
         }
         return result;
@@ -61,35 +61,35 @@ public class AreaUnits implements Units {
     public double convertCommonToAnswer(String unit, Double value){
         double result = 0.0;
         switch(unit){
-            case "Square Inch":
-                result = value / 0.00064516;
-                break;
-            case "Square Foot":
-                result = value / 0.09290304;
-                break;
-            case "Square Yard":
-                result = value / 0.83612736;
-                break;
-            case "Square Mile":
-                result = value / 2589988.110336;
-                break;
             case "Acre":
                 result = value / 4046.8564224;
                 break;
             case "Hectare":
                 result = value / 10000;
                 break;
-            case "Square Millimeter":
-                result = value / 0.000001;
-                break;
             case "Square Centimeter":
                 result = value / 0.0001;
+                break;
+            case "Square Foot":
+                result = value / 0.09290304;
+                break;
+            case "Square Inch":
+                result = value / 0.00064516;
+                break;
+            case "Square Kilometer":
+                result = value / 1000000;
                 break;
             case "Square Meter":
                 result = value;
                 break;
-            case "Square Kilometer":
-                result = value / 1000000;
+            case "Square Mile":
+                result = value / 2589988.110336;
+                break;
+            case "Square Millimeter":
+                result = value / 0.000001;
+                break;
+            case "Square Yard":
+                result = value / 0.83612736;
                 break;
         }
         return result;
